0c8be0207dd030a2ef8624d6e12adef9f6ef9b37
jijiajia19 <jijiajia19@gmail.com>
2021-12-15 15:18:39 +0800

    基础initlizer提交

---
+++ src/main/java/com/jacle/springbootv2/springbootv2/Springbootv2Application.java
@@ -0,0 +1,15 @@
+package com.jacle.springbootv2.springbootv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootv2Application
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Springbootv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacle/springbootv2/springbootv2/Springbootv2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jacle.springbootv2.springbootv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootv2ApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
